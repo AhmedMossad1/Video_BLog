b0d373de2b55b6c630c3c2942ccd2c11b8416b91
AhmedMossad1 <ahmedmosad525@gmail.com>
2024-01-14 10:49:38 +0200

    Api for video Class

---
+++ app/Http/Controllers/Api/VideoController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BackEnd\Videos\Store;
+use App\Http\Resources\VideoResource;
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller
+{
+    public function index(){
+        $video = VideoResource::collection(Video::get());
+        $msg = ['ok'];
+        return response($video,200,$msg);
+    }
+    public function show($id){
+        $video = new VideoResource(Video::find($id));
+        $msg = ['ok'];
+        return response($video,200,$msg);
+
+    }
+    public function store(Store $request){
+        $video = Video::create($request->all());
+        $message1 = ['created'];
+        $message2 = ['not Saved'];
+        if($video){
+            return response(new VideoResource($video),201,$message1);
+        }
+        return response(new VideoResource($video),400,$message2);
+    }
+    public function update(Request $request,$id){
+        $video = Video::find($id);
+        $video->update($request->all());
+        $msg = ['Ok'];
+        $msg2 = ['Not Save'];
+        if($video){
+        return response(new VideoResource($video),201,$msg);
+        }
+        return response(new VideoResource($video),400,$msg2);
+    }
+    public function destory($id){
+        $video = Video::find($id);
+        $video->delete($id);
+        $msg = ['Deleted'];
+        $msg2 = ['Not Saved'];
+        if($video){
+        return response(new VideoResource($video),201,$msg);
+        }
+        return response(new VideoResource($video),400,$msg2);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\VideoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/video',[VideoController::class,'index']);
+Route::get('/video/{id}',[VideoController::class,'show']);
+Route::post('/video',[VideoController::class,'store']);
+Route::post('/video/{id}',[VideoController::class,'update']);
+Route::post('/videos/{id}',[VideoController::class,'destory']);
